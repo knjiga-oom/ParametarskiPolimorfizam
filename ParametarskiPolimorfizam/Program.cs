320c0175f5c76e825a79c87e400840cea83fc03f
Julijan Šribar <julijan.sribar@gmail.com>
2017-06-18 13:52:43 +0200

    Add project files.

---
+++ ParametarskiPolimorfizam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ParametarskiPolimorfizam
+{
+    class MojTip<T>
+    {
+        public MojTip(T vrijednost)
+        {
+            this.vrijednost = vrijednost;
+        }
+
+        public MojTip<U> Promijeni<U>(Func<T, U> funkcija)
+        {
+            return new MojTip<U>(funkcija(vrijednost));
+        }
+        // dodano radi jednostavnijeg ispisa
+        public override string ToString()
+        {
+            return vrijednost.ToString();
+        }
+
+        public readonly T vrijednost;
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var a = new MojTip<int>(5);
+            Console.WriteLine(a);
+            var b = a.Promijeni(v => v * 0.5);
+            Console.WriteLine(b);
+            var c = a.Promijeni(v => Enum.ToObject(typeof(DayOfWeek), v));
+            Console.WriteLine(c);
+        }
+    }
+}
